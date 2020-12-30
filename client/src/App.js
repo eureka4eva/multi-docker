6d53dabc55cbe51b35d8dfd1488be5fb5f8c266a
eureka4eva <eureka4eva@yahoo.co.uk>
2020-12-30 13:31:12 +0100

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  
+//class App extends Component {
+  //render(){
+     return (
+   <Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+        <Link to="/">Home</Link>
+        <Link to="/OtherPage">Other Page</Link>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" Component={OtherPage} />
+        </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+   </Router>   
+  );
+
+}
+export default App;
